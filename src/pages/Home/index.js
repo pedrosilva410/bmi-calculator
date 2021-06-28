f2594508254427377de40e347376d82e39f1b01d
Pedro <35297069+pedrosilva410@users.noreply.github.com>
2021-06-28 11:30:25 +0200

    Barebones

---
+++ src/pages/Home/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+
+const Home = () => {
+  return (
+    <main>
+      <h1>BMI</h1>
+      <p className="home__p">
+        Body mass index
+      </p>
+    </main>
+  );
+};
+
+export default Home;
