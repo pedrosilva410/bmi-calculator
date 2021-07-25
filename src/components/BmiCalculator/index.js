4db95beb5021c3057362763d6a826503dad1d727
Pedro <35297069+pedrosilva410@users.noreply.github.com>
2021-07-25 21:35:27 +0200

    created the calculator function

---
+++ src/components/BmiCalculator/index.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { useState } from "react";
+import "./styles.css";
+
+const BmiCalculator = () => {
+
+  const [height, setHeight] = useState("")
+  const [weight, setWeight] = useState("")
+
+  let handleClick = () => {
+    console.log(weight/((height/100)*(height/100)))
+  }
+
+
+  return (
+    <main>
+      <h1>BMI Calculator</h1>
+
+      <p className="bmi__p">
+        insert your data
+      </p>
+
+      <div>
+
+        <input
+            onChange={(event) => {
+              setHeight(event.target.value);
+            }}
+            value={height}
+            className="bmi__height"
+            type="text"
+            placeholder="170"
+        />
+
+        <label>cm</label>
+
+      </div>
+
+      <div>
+
+        <input
+          onChange={(event) => {
+            setWeight(event.target.value);
+          }}
+          value={weight}
+          className="bmi__weight"
+          type="text"
+          placeholder="70"
+        />
+
+        <label>kg</label>
+
+      </div>
+
+      <button onClick={handleClick}>calculate</button>
+    </main>
+  );
+};
+
+export default BmiCalculator;
--- src/pages/BMI/index.js
+++ src/pages/BMI/index.js
@@ -1,13 +1,11 @@
 import React from "react";
 import "./styles.css";
+import BmiCalculator from "../../components/BmiCalculator";
 
 const BMI = () => {
   return (
     <main>
-      <h1>BMI Calculator</h1>
-      <p className="bmi__p">
-        insert your data
-      </p>
+      <BmiCalculator/>
     </main>
   );
 };
